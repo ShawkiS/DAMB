1835a174f02900bb4ccd15768e6b528ea70ca76c
ShawkiS <shawki@oceanprotocol.com>
2019-12-17 12:38:40 +0200

    Add Truffle-React box

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import getWeb3 from "./getWeb3";
+
+import "./App.css";
+
+class App extends Component {
+  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+
+  render() {
+    <h1>Hello:)</h1>
+  }
+}
+
+export default App;
